42e98769c7cf2e89e0baef7557c262f7c2746a89
emoacht <emotom@pobox.com>
2015-01-30 23:25:22 +0900

    Initial commit

---
+++ ManagedNativeWifi/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagedNativeWifi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (!Debugger.IsAttached)
+                Debug.Listeners.Add(new TextWriterTraceListener(Console.Out));
+
+            Debug.WriteLine("[Available Networks]");
+            NativeWifi.GetAvailableNetworkSsid().ToArray();
+
+            Debug.WriteLine("[Connected Networks]");
+            NativeWifi.GetConnectedNetworkSsid().ToArray();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+﻿Managed Native Wifi
+===================
+
+A managed implementation of Native Wifi API
+
+##Requirements
+
+ * .NET Framework 4.0
+
+##License
+
+ - MIT License
