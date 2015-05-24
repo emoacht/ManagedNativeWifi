c86f943e0c58885bb22443f4214490e34815e035
emoacht <emotom@pobox.com>
2015-05-24 12:51:01 +0900

    Added comment

--- ManagedNativeWifi/Program.cs
+++ ManagedNativeWifi/Program.cs
@@ -7,18 +7,18 @@ using System.Threading.Tasks;
 
 namespace ManagedNativeWifi
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            if (!Debugger.IsAttached)
-                Debug.Listeners.Add(new TextWriterTraceListener(Console.Out));
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			if (!Debugger.IsAttached)
+				Debug.Listeners.Add(new TextWriterTraceListener(Console.Out));
 
-            Debug.WriteLine("[Available Networks]");
-            NativeWifi.GetAvailableNetworkSsid().ToArray();
+			Debug.WriteLine("[Available Networks]");
+			NativeWifi.GetAvailableNetworkSsid().ToArray();
 
-            Debug.WriteLine("[Connected Networks]");
-            NativeWifi.GetConnectedNetworkSsid().ToArray();
-        }
-    }
+			Debug.WriteLine("[Connected Networks]");
+			NativeWifi.GetConnectedNetworkSsid().ToArray();
+		}
+	}
 }
