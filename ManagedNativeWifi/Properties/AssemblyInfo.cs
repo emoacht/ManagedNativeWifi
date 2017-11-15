fac5f7298e8eb472c7a88768f331ea44393738b0
emoacht <emotom@pobox.com>
2017-11-16 07:27:04 +0900

    Incremented minor version

--- ManagedNativeWifi/Properties/AssemblyInfo.cs
+++ ManagedNativeWifi/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ManagedNativeWifi")]
-[assembly: AssemblyCopyright("Copyright © 2015 EMO")]
+[assembly: AssemblyCopyright("Copyright © 2015-2017 emoacht")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,8 +32,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.2")]
-[assembly: AssemblyFileVersion("1.1.0.2")]
+[assembly: AssemblyVersion("1.2.0.4")]
+[assembly: AssemblyFileVersion("1.2.0.4")]
 
 // For unit test
 [assembly: InternalsVisibleTo("ManagedNativeWifi.Test")]
