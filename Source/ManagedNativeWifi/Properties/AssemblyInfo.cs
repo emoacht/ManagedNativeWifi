88678aa9c078c2268ebc52436c1cea02be5430a6
emoacht <emotom@pobox.com>
2019-11-29 21:32:35 +0900

    Increment minor version

--- Source/ManagedNativeWifi/Properties/AssemblyInfo.cs
+++ Source/ManagedNativeWifi/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ManagedNativeWifi")]
-[assembly: AssemblyCopyright("Copyright © 2015-2018 emoacht")]
+[assembly: AssemblyCopyright("Copyright © 2015-2019 emoacht")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,8 +33,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.4.0.0")]
-[assembly: AssemblyFileVersion("1.4.0.0")]
+[assembly: AssemblyVersion("1.5.0.0")]
+[assembly: AssemblyFileVersion("1.5.0.0")]
 [assembly: NeutralResourcesLanguage("en-US")]
 
 // For unit test
