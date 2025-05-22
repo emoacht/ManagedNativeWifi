96267d09567f479c1121322c2ad21435fe7fc809
Benoit Bergeron <bbergeron+eddyfi@eddyfi.com>
2025-05-21 21:33:47 -0400

    Add method GetConnectionQualityInfo

---
+++ Source/ManagedNativeWifi/ConnectionQualityLinkInfo.cs
@@ -0,0 +1,39 @@
+﻿namespace ManagedNativeWifi;
+
+/// <summary>
+/// Information about an individual radio link in a wireless connection
+/// </summary>
+public class ConnectionQualityLinkInfo
+{
+	/// <summary>
+	/// Link ID
+	/// </summary>
+	public byte LinkId { get; }
+
+	/// <summary>
+	/// Received Signal Strength Indicator (dBm)
+	/// </summary>
+	public int Rssi { get; }
+
+	/// <summary>
+	/// Channel Center frequency (KHz)
+	/// </summary>
+	public int Frequency { get; }
+
+	/// <summary>
+	/// Bandwidth (MHz)
+	/// </summary>
+	public int Bandwidth { get; }
+
+	internal ConnectionQualityLinkInfo(
+		byte linkId,
+		int rssi,
+		int frequency,
+		int bandwidth)
+	{
+		LinkId = linkId;
+		Rssi = rssi;
+		Frequency = frequency;
+		Bandwidth = bandwidth;
+	}
+}
